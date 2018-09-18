81f123ffb46e59eaf8d8ac52e937fc5561c23787
kakik <39730096+kakik@users.noreply.github.com>
2018-09-18 18:49:46 +0900

    Drill 05 Real commit_02
    add functions

---
+++ Drills/Drill 05 Real/Drill 05 Real.py
@@ -0,0 +1,31 @@
+
+from pico2d import *
+
+import math
+
+open_canvas()
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+
+def  move_point_to_point(from_x, from_y, to_x, to_y)
+    pass
+
+
+while True:
+    move_point_to_point()
+    move_point_to_point()
+    move_point_to_point()
+    move_point_to_point()
+    move_point_to_point()
+    move_point_to_point()
+    move_point_to_point()
+    move_point_to_point()
+    move_point_to_point()
+    move_point_to_point()
+
+
+
+
+close_canvas()
